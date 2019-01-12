dfdf386e82c8f524d89ddc6808a825f229067317
mrmodise <modisemorebodi@gmail.com>
2019-01-12 12:42:53 +0200

    updated styling for listing heroes. Changed styling from scss to css

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,18 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import {HEROES} from '../mock-heroes';
+import {Hero} from '../hero';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.scss']
+  styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
 
   heroes = HEROES;
+  selectedHero: Hero;
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
   }
 
+  onSelect(hero: Hero) {
+    this.selectedHero = hero;
+  }
 }
