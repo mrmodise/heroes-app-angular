a8452e525c1be6a6865a21522345870507135b7a
mrmodise <modisemorebodi@gmail.com>
2019-01-12 14:16:28 +0200

    added hero service

---
+++ src/app/hero.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import {HEROES} from './mock-heroes';
+import {Observable, of} from 'rxjs';
+import {Hero} from './hero';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HeroService {
+
+  constructor() { }
+
+  getHeroes(): Observable<Hero[]> {
+    return of(HEROES);
+  }
+}
