3b3e7fb1c775163dfbaed7c902e89a11bd9be2b8
mrmodise <modisemorebodi@gmail.com>
2019-01-13 16:28:30 +0200

    loaded dashboard component into the router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,17 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
 import {HeroesComponent} from './components/heroes/heroes.component';
 import {DashboardComponent} from './components/dashboard/dashboard.component';
 
 const routes: Routes = [
   {path: 'heroes', component: HeroesComponent},
-  {path: '', component: DashboardComponent}
+  {path: 'dashboard', component: DashboardComponent},
+  {path: '', redirectTo: '/dashboard', pathMatch: 'full'}
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
